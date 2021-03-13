17dbb90ec49e1d382c0dce09db10c902ae254394
MadHatter0616 <56076176+MadHatter0616@users.noreply.github.com>
2021-03-13 09:45:28 +0800

    Menu commit

---
+++ Menu.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <iostream>
+void Menu_UI_oneplayer()
+{	
+	std::cout << "****************************************************" << std::endl;
+	std::cout << "*    Welcome to Bulls and Cows, a fun word game.   *" << std::endl;
+	std::cout << "*                                                  *" << std::endl;
+	std::cout << "*              }   {         ___                   *" << std::endl;
+	std::cout << "*              (o o)        (o o)                  *" << std::endl;
+	std::cout << "*       /-------\\ /          \\ /-------\\           *" << std::endl;
+	std::cout << "*      / | BULL |O            O| COW  | \\          *" << std::endl;
+	std::cout << "*     *  |-,--- |              |------|  *         *" << std::endl;
+	std::cout << "*        ^      ^              ^      ^            *" << std::endl;
+	std::cout << "*                                                  *" << std::endl;
+	std::cout << "*          Choose the number of players            *" << std::endl;
+	std::cout << "*                                                  *" << std::endl;
+	std::cout << "*                 ->  1 Player                     *" << std::endl;
+	std::cout << "*                     2 Player                     *" << std::endl;
+	std::cout << "****************************************************" << std::endl;
+
+	return;
+}
+
+void Menu_UI_twoplayer()
+{
+	std::cout << "****************************************************" << std::endl;
+	std::cout << "*    Welcome to Bulls and Cows, a fun word game.   *" << std::endl;
+	std::cout << "*                                                  *" << std::endl;
+	std::cout << "*              }   {         ___                   *" << std::endl;
+	std::cout << "*              (o o)        (o o)                  *" << std::endl;
+	std::cout << "*       /-------\\ /          \\ /-------\\           *" << std::endl;
+	std::cout << "*      / | BULL |O            O| COW  | \\          *" << std::endl;
+	std::cout << "*     *  |-,--- |              |------|  *         *" << std::endl;
+	std::cout << "*        ^      ^              ^      ^            *" << std::endl;
+	std::cout << "*                                                  *" << std::endl;
+	std::cout << "*          Choose the number of players            *" << std::endl;
+	std::cout << "*                     1 Player                     *" << std::endl;
+	std::cout << "*                 ->  2 Player                     *" << std::endl;
+	std::cout << "****************************************************" << std::endl;
+
+	return;
+}
